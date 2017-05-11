eef5663b22dd35b73b13028a25f1a19d8f010d0b
JulieSway0206 <cui.jie0206@gmail.com>
2017-05-11 16:59:05 -0400

    changes to be fetched

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ module.exports = function(app)
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds031561.mlab.com:31561/heroku_bf49z5k7'; // user yours
     }
 
     var mongoose = require("mongoose");
