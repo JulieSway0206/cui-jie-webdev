0d44046b5c4c555c93c35db8ad3b14b0d7ca07ac
JulieSway0206 <cui.jie0206@gmail.com>
2017-05-28 15:04:46 -0400

    try widgetlistcontroller

---
+++ public/assignment/services/widget.service.client.js
@@ -0,0 +1,62 @@
+/**
+ * Created by SeedofWind on 5/28/17.
+ */
+(function () {
+    angular
+        .module('WebAppMaker')
+        .service('widgetService', widgetService);
+
+    function widgetService() {
+        this.findAllWidgetsForPage = findAllWidgetsForPage;
+        // this.findwidgetById = findwidgetById;
+        // this.deletewidget = deletewidget;
+        // this.createwidget = createwidget;
+
+        var widgets = [
+            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
+            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                "url": "http://lorempixel.com/400/200/"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": '<p>On Friday, Uber CEO Travis Kalanick’s mother, Bonnie Kalanick, was killed in a <a href="http://abc30.com/news/boating-accident-at-pine-flat-lake-leaves-woman-dead-husband-injured/2041838/" target="_blank" rel="noopener">boating accident</a> at Pine Flat Lake in California. His father, Donald Kalanick was also involved in the accident and was <a href="http://www.businessinsider.com/travis-kalanick-parents-bonnie-donald-boating-accident-2017-5" target="_blank" rel="noopener">reportedly</a> taken to a local hospital in “serious condition.”</p>'},
+            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+        ];
+
+
+
+        // function createwidget(widget){
+        //     widget._id = (new Date()).getTime() + "";
+        //     widgets.push(widget);
+        // }
+        //
+        //
+        //
+        // function deletewidget(widgetId) {
+        //     var widget = findwidgetById(widgetId);
+        //     var index = widgets.indexOf(widget);
+        //     widgets.splice(index, 1);
+        // }
+
+
+
+        // function findwidgetById(widgetId) {
+        //     return widgets.find(function (widget) {
+        //         return widget._id === widgetId;
+        //     });
+        //
+        // }
+
+        function findAllWidgetsForPage(pageId) {
+            var results = [];
+            for(var v in widgets) {
+                if(widgets[v].pageId === pageId){
+                    results.push(widgets[v]);
+                }
+            }
+
+            return results;
+        }
+    }
+})();
--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -6,23 +6,23 @@
         .module('WebAppMaker')
         .controller('widgetListController', widgetListController);
 
-    function widgetListController($sce) {
+    function widgetListController($sce,$routeParams, widgetService) {
         var model = this;
-        var widgets = [
-            { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
-            { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://lorempixel.com/400/200/"},
-            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": '<p>On Friday, Uber CEO Travis Kalanick’s mother, Bonnie Kalanick, was killed in a <a href="http://abc30.com/news/boating-accident-at-pine-flat-lake-leaves-woman-dead-husband-injured/2041838/" target="_blank" rel="noopener">boating accident</a> at Pine Flat Lake in California. His father, Donald Kalanick was also involved in the accident and was <a href="http://www.businessinsider.com/travis-kalanick-parents-bonnie-donald-boating-accident-2017-5" target="_blank" rel="noopener">reportedly</a> taken to a local hospital in “serious condition.”</p>'},
-            { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
-            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
-        ];
 
-        model.widgets = widgets;
         model.trust = trust;
         model.getYouTubeEmbedUrl = getYouTubeEmbedUrl;
+        model.widgetUrl = widgetUrl;
+        model.pageId = $routeParams['pageId']
+
+        function init() {
+            model.widgets = widgetService.findAllWidgetsForPage(model.pageId);
+        }
+        init();
+
+        function widgetUrl(widget) {
+            var url = 'views/widget/templates/widget-'+widget.widgetType.toLowerCase()+'.view.client.html';
+            return url;
+        }
 
         function getYouTubeEmbedUrl(linkUrl) {
              var embedUrl = "https://www.youtube.com/embed/";
