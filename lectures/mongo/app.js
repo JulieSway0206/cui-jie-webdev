ddbd8c4ac5190d66ef62836200d87cf38a846ae7
JulieSway0206 <cui.jie0206@gmail.com>
2017-06-12 02:58:20 -0400

    bad heroku

--- lectures/mongo/app.js
+++ lectures/mongo/app.js
@@ -9,12 +9,7 @@ mongoose.Promise = require('q').Promise;
 var connectionString = 'mongodb://localhost/webdev'; // for local
 
 
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds031561.mlab.com:31561/heroku_bf49z5k7'; // user yours
-}
+
 
 mongoose.connect(connectionString);
 
