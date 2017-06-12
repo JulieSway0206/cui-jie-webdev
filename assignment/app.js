724cc142d9b896532c2997391e256140be2c4d7d
JulieSway0206 <cui.jie0206@gmail.com>
2017-06-12 10:20:56 -0400

    again heroku

--- assignment/app.js
+++ assignment/app.js
@@ -3,8 +3,21 @@
  */
 var app = require('../express');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webdev');
 mongoose.Promise = require('q').Promise;
+
+var connectionString = 'mongodb://localhost/webdev'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds031561.mlab.com:31561/heroku_bf49z5k7'; // user yours
+}
+
+
+
+
+mongoose.connect(connectionString);
+
 
 
 
