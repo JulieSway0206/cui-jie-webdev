fdde470e56c695c8f01cad4060bcbda924dddab1
JulieSway0206 <cui.jie0206@gmail.com>
2017-06-25 18:43:48 -0400

    add key

--- public/project/services/search.service.client.js
+++ public/project/services/search.service.client.js
@@ -10,7 +10,7 @@
 
         this.searchBook= searchBook;
 
-        var urlBase = url = "https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=1";
+        var urlBase = url = "https://www.googleapis.com/books/v1/volumes?q=harry+potter&maxResults=1&key=AIzaSyCLqXiQxl0SiQjSrxmD7ENw8Fmv9FY7xR4";
 
 
         function searchBook(searchText) {
